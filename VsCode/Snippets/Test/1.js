389312abfad4de153621593fbdba9be3027d267a
cc <home@iwenli.org>
2019-04-14 01:24:31 +0800

    snippets

--- VsCode/Snippets/Test/1.js
+++ VsCode/Snippets/Test/1.js
@@ -1,9 +1,64 @@
-
+//https://code.visualstudio.com/docs/editor/intellisense
 var Fun = {
-    name:'张三',
-    show: function () {
-        alert( `${this.name} asy`);
+    name: '张三',
+    show: function() {
+        alert(`${this.name} asy`);
     },
 };
 
 Fun.show();
+
+let name = value;
+
+var obj = { a: 1, b: 2 };
+let { a } = obj;
+// alert(a);
+console.log(a);
+
+const name = value;
+((arguments) => {
+
+})();
+
+((arguments) => {
+
+})();
+
+fn.apply(this, arguments)
+fn.call(this, arguments)
+fn.bind(this, arguments)
+after(() => {
+
+});
+
+[...Array(1, 2, 3).keys()]
+
+setTimeout(() => {
+
+}, delay);
+setTimeout(() => {
+    if (1 == 2) {
+
+    }
+}, 1);
+
+setTimeout(() => {
+    console.log(123);
+}, delay);
+
+
+'use strict';
+process.nextTick(() => {
+
+});
+
+console.log();
+console.groupEnd();
+class name extends base {
+    constructor(arguments) {
+        super(arguments);
+
+    }
+}
+console.timeEnd(object);
+E > name
